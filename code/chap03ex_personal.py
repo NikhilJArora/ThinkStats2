561d35bae4a50f01b4f4e2648353be1f7144efc2
NikhilJArora <nikhiljarora@outlook.com>
2017-08-26 21:07:05 -0400

    Added notebook that comutes a bias and un bias plot

---
+++ code/chap03ex_personal.py
@@ -0,0 +1,32 @@
+"""
+    By: Nikhil Arora
+    Email: nikhiljarora@outlook.com
+
+
+    File: stores procedures to compute the bias and un bias plots of
+    Pmf (prob mass function)
+
+
+"""
+
+from __future__ import print_function, division
+
+import numpy as np
+
+import nsfg
+import first
+import thinkstats2
+import thinkplot
+
+#
+def makeBiasPmf(Pmf, label):
+        bias_pmf = Pmf.Copy()
+        
+        return bias_pmf
+
+
+
+def PmfMean(Pmf):
+    Pmf.mean()
+
+    return mean
